6556e6ab236213da7061683c6556f648933f10c2
Gaiety <learningisalife@gmail.com>
2024-09-21 09:21:48 +0300

    Adds ability to switch between denominator and numerator

---
+++ timeTable.js
@@ -0,0 +1,158 @@
+  // Знаменник
+const denominationDays = [
+    {
+        name: "Понеділок",
+        dayNumber: 1,
+        lections: [
+            {
+                teacher: "-",
+                discipline: "-",
+                link: null,
+            },
+            {
+                teacher: "Шаравара В.В",
+                discipline: "Agile",
+                link: "https://us05web.zoom.us/j/89552674199?pwd=NoklBOAxqLvQ5Qbt0ER49oSBvy73VZ.1",
+            },
+        ],
+    },
+    {
+        name: "Вівторок",
+        dayNumber: 2,
+        lections: [
+            {
+                teacher: "Волкова С.І.",
+                discipline: "Онтологічне забезпечення web-систем",
+                link: "https://discord.gg/2y9AG225ym",
+            },
+            {
+                teacher: "Волкова С.І.",
+                discipline: "Онтологічне забезпечення web-систем",
+                link: "https://discord.gg/2y9AG225ym",
+            },
+            {
+                teacher: "Івченко Ю.М.",
+                discipline: "Пргорам.забезп.комп.мереж",
+                link: "https://us05web.zoom.us/j/84118575752?pwd=NXUyYUtvZW9iKzBVYmczMEdHMlRsZz09",
+            },
+            {
+                teacher: "Івченко Ю.М.",
+                discipline: "Стандартизація прогр.систем",
+                link: "https://us05web.zoom.us/j/84118575752?pwd=NXUyYUtvZW9iKzBVYmczMEdHMlRsZz09",
+            },
+        ],
+    },
+    {
+        name: "Четвер",
+        dayNumber: 4,
+        lections: [
+            {
+                teacher: "Шаравара В.В",
+                discipline: "Семантичний web",
+                link: "https://us05web.zoom.us/j/89552674199?pwd=NoklBOAxqLvQ5Qbt0ER49oSBvy73VZ.1",
+            },
+            {
+                teacher: "Мунтян А.О.",
+                discipline: "Ділове (наук.) спілкув.ін.мовою",
+                link: "https://us05web.zoom.us/j/99136469020?pwd=NGhUc05ZQ05wQzlycVRPRldWZjRyQT09",
+            },
+            {
+                teacher: "-",
+                discipline: "-",
+                link: null,
+            },
+            {
+                teacher: "Горбова О.В.",
+                discipline: "Комп.дизайн та 3-d-моделювання",
+                link: "https://us04web.zoom.us/j/7445088241?pwd=ak1XbmprdFBUQnRZOWszWU91VHdyUT09",
+            },
+            {
+                teacher: "Горбова О.В.",
+                discipline: "Комп.дизайн та 3-d-моделювання",
+                link: "https://us04web.zoom.us/j/7445088241?pwd=ak1XbmprdFBUQnRZOWszWU91VHdyUT09",
+            },
+        ],
+    },
+];
+
+// Чисельник
+const days = [
+    {
+        name: "Понеділок",
+        dayNumber: 1,
+        lections: [
+            {
+                teacher: "-",
+                discipline: "-",
+                link: null,
+            },
+            {
+                teacher: "Шаравара В.В",
+                discipline: "Agile",
+                link: "https://us05web.zoom.us/j/89552674199?pwd=NoklBOAxqLvQ5Qbt0ER49oSBvy73VZ.1",
+            },
+            {
+                teacher: "Шаравара В.В",
+                discipline: "Сучасні освітні технології",
+                link: "https://us05web.zoom.us/j/89552674199?pwd=NoklBOAxqLvQ5Qbt0ER49oSBvy73VZ.1",
+            },
+            {
+                teacher: "Шаравара В.В",
+                discipline: "Сучасні освітні технології",
+                link: "https://us05web.zoom.us/j/89552674199?pwd=NoklBOAxqLvQ5Qbt0ER49oSBvy73VZ.1",
+            },
+        ],
+    },
+    {
+        name: "Вівторок",
+        dayNumber: 2,
+        lections: [
+            {
+                teacher: "-",
+                discipline: "-",
+                link: null,
+            },
+            {
+                teacher: "Івченко Ю.М.",
+                discipline: "Стандартизація прогр.систем",
+                link: "https://us05web.zoom.us/j/84118575752?pwd=NXUyYUtvZW9iKzBVYmczMEdHMlRsZz09",
+            },
+            {
+                teacher: "Івченко Ю.М.",
+                discipline: "Пргорам.забезп.комп.мереж",
+                link: "https://us05web.zoom.us/j/84118575752?pwd=NXUyYUtvZW9iKzBVYmczMEdHMlRsZz09",
+            },
+        ],
+    },
+    {
+        name: "Четвер",
+        dayNumber: 4,
+        lections: [
+            {
+                teacher: "Шаравара В.В",
+                discipline: "Семантичний web",
+                link: "https://us05web.zoom.us/j/89552674199?pwd=NoklBOAxqLvQ5Qbt0ER49oSBvy73VZ.1",
+            },
+            {
+                teacher: "Мунтян А.О.",
+                discipline: "Ділове (наук.) спілкув.ін.мовою",
+                link: "https://us05web.zoom.us/j/99136469020?pwd=NGhUc05ZQ05wQzlycVRPRldWZjRyQT09",
+            },
+            {
+                teacher: "-",
+                discipline: "-",
+                link: null,
+            },
+            {
+                teacher: "Горбова О.В.",
+                discipline: "Комп.дизайн та 3-d-моделювання",
+                link: "https://us04web.zoom.us/j/7445088241?pwd=ak1XbmprdFBUQnRZOWszWU91VHdyUT09",
+            },
+            {
+                teacher: "Горбова О.В.",
+                discipline: "Комп.дизайн та 3-d-моделювання",
+                link: "https://us04web.zoom.us/j/7445088241?pwd=ak1XbmprdFBUQnRZOWszWU91VHdyUT09",
+            },
+        ],
+    },
+];
